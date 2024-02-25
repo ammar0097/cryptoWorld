8fa8b144a672cd63f1d174738fb9274552d8e5e5
Ammar <ammarferchichi7@gmail.com>
2024-02-25 14:16:34 +0100

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/app.component.{html,ts}",
+    "./src/app/header/header.component.{html,ts}",
+
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
